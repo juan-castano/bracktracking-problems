f798225456e01e2b4319cd32670e1a55782558e1
Juan Castaño <[jucacase92@gmail.com]>
2015-04-17 05:42:56 -0500

    ligths-games folder with initial file

---
+++ lights-games/ligths-games.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Idea is change the state of ligths on NxN matrix from zeros, on up,down,left,rigth postitions
+and center point too.
+"""
+
+def show_solution(matrix):
+
+    for row in matrix:
+        print(row)
+
+    print("\n")
+
+
+def main():
+
+    matrix = [
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+    ]
+
+
+
+if __name__ == '__main__':
+    main()
