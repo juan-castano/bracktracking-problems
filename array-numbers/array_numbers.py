f6d6d483c6c1e00bfb5d1661ce7224df61531aa8
Juan Castaño <[jucacase92@gmail.com]>
2015-04-22 17:46:11 -0500

    Problema de vector de numeros solucionado

--- array-numbers/array_numbers.py
+++ array-numbers/array_numbers.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
-def show_numbers(array):
 
-    if len(array) >= 0:
-        print(array)
-        array = [1:]
-        show_numbers(array)
+def __show_numbers(array, number, value):
 
-    else:
-        return
+    if len(array) > 0:
+        if number != array[0]:
+            if (number+array[0]) == value:
+                print("%s + %s = %s" % (number, array[0], value))
+
+        __show_numbers(array[1:], number, value)
+
+
+def show_numbers(array, value):
+
+    for number in array:
+        __show_numbers(array, number, value)
+
+
 
 
 def main():
-    array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    show_numbers(array)
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    number = int(input("Ingrese el numero a buscar: "))
+    show_numbers(array, number)
+
 
 if __name__ == '__main__':
     main()
